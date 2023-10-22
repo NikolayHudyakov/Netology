c6f60d93ca4f6b815e7ec8c512ff2a82cb7a4e75
Nikolay <nikolayhudyakov18@gmail.com>
2023-10-22 17:49:55 +0400

    make m2m-relations

--- dj-homeworks/2.2-databases-2/m2m-relations/articles/admin.py
+++ dj-homeworks/2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     pass
+
+
+class ScopeInlineFormSet(BaseInlineFormSet):
+    pass
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 0
+    formset = ScopeInlineFormSet
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+
+
