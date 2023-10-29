521b4ecb47a534228b9dca17d3a31779ae434936
Nikolay <nikolayhudyakov18@gmail.com>
2023-10-29 21:08:43 +0400

    create models and serializes

--- dj-homeworks/3.1-drf-intro/smart_home/measurement/views.py
+++ dj-homeworks/3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,18 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+from measurement.models import Sensor
+from measurement.serializers import SensorDetailSerializer
+
+
+class SensorView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def post(self, request: Request):
+
+        return Response({'status': 'ok'})
+
