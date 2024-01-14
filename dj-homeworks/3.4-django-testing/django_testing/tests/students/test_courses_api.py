9f8c694b42b13491596cdcf0cf3a73e2bcc146e8
Nikolay <nikolayhudyakov18@gmail.com>
2024-01-14 17:27:34 +0400

    test_get_course

--- dj-homeworks/3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ dj-homeworks/3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,34 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from rest_framework.test import APIClient
+from model_bakery import baker
+from students.models import Student, Course
+
+
+@pytest.fixture()
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+    return factory
+
+
+@pytest.mark.django_db
+def test_get_course(client, course_factory):
+    course = course_factory(_quantity=1)[0]
+
+    response = client.get('/api/v1/courses/0')
+
+    data = response.json()
+
+    assert course == data
